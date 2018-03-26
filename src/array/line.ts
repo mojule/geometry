3b733d0ff0f3635bdea1b65b536c383e89051a21
Nik Coughlin <nrkn.com@gmail.com>
2018-03-26 17:19:19 +1300

    Playing with simple array implementations

---
+++ src/array/line.ts
@@ -0,0 +1,19 @@
+import { Point } from "./point";
+
+export type Line = [ number, number, number, number ]
+
+export const intersection = ( x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, x4: number, y4: number  ): Point | undefined => {
+  const denom = ( y4 - y3 ) * ( x2 - x1 ) - ( x4 - x3 ) * ( y2 - y1 )
+
+  if( !denom ) return
+
+  const ua = ( ( x4 - x3 ) * ( y1 - y3 ) - ( y4 - y3 ) * ( x1 - x3 ) ) / denom
+  const ub = ( ( x2 - x1 ) * ( y1 - y3 ) - ( y2 - y1 ) * ( x1 - x3 ) ) / denom
+  const x = x1 + ua * ( x2 - x1 )
+  const y = y1 + ua * ( y2 - y1 )
+
+  return [ x, y ]
+}
+
+export const vector = ( x1: number, y1: number, x2: number, y2: number ): Point =>
+  [ x2 - x1, y2 - y1 ]
